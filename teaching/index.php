4838499c41a0a3a2dd4f2c790679c53fc32e92ec
Steve Cholewiak <steven@cholewiak.com>
2021-10-19 14:17:19 -0700

    Initial commit of original content

---
+++ teaching/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Teaching - Steven A. Cholewiak, Ph.D.</title>
+    <?php include_once("../insert_header.php") ?>
+    <style>
+        #nav #teaching { background-position: 0px -3.6vw }
+        @media screen and (min-width: 900px) {
+            #nav #teaching { background-position: 0px -32px }
+        }
+    </style>
+</head>
+
+<body>
+    <?php include_once("../insert_sidebar.php") ?>
+    <div id="content">
+        <div id="content_body">
+            <?php include_once("content.html") ?>
+        </div>
+    </div>
+    <?php include_once("../insert_footer.php") ?>
+</body>
+</html>
